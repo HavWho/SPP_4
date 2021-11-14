43066f70bfe6c86b0a65a3cd63ef22017b844c35
HavWho <workplacer2019@gmail.com>
2021-11-15 02:38:51 +0300

    add task

---
+++ TestsGenerator/MainApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using TestsGeneratorLib;
+
+namespace TestsGenerator
+{
+    internal static class MainApp
+    {
+        public static void Main()
+        {
+
+            //var genPath = Path.GetFullPath(@"..\..\..\..\TestsGenerator\Tests\GenClasses\");
+            const string testPath = @"..\..\Classes\\GeneratedTests\\";
+            var collection = new List<string>
+            {
+                @"..\..\Classes\\ClassFormatter.cs",
+                @"..\..\Classes\\ConstructorFormatter.cs",
+                @"..\..\Classes\\\FieldFormatter.cs",
+                @"..\..\Classes\\MethodFormatter.cs",
+                @"..\..\Classes\\PropertiesFormatter.cs"
+            };
+            var generator = new NUnitTestsGenerator();
+            try
+            {
+                var task = generator.GenerateCLasses(collection, testPath, 1, 1,1);
+                task?.Wait();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            Console.WriteLine("Finish");
+            Console.ReadLine();
+        }
+    }
+}
